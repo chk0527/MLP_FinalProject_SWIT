3207166ced1fa8e4f1e07541e29c7e080bf5f21f
kim-youngwook <rfonline64@gmail.com>
2024-05-24 10:08:55 +0900

    frist user table commit

---
+++ back/swit/src/test/java/com/swit/repository/UserRepositoryTest.java
@@ -0,0 +1,42 @@
+package com.swit.repository;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.swit.domain.User;
+
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+@SpringBootTest
+public class UserRepositoryTest {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    public void test() {
+        log.info("---------------------");
+        log.info(userRepository);
+    }
+
+    @Test
+    public  void testInsert(){
+        for (int i=1; i<=100; i++){
+            User user = User.builder()
+            .user_id("user"+i).user_email("user@swit" + i + ".com")
+            .user_name("김철수" + i).user_password("1234")
+            .user_phone("010-1234-1234").user_nick("슈퍼맨")
+            .user_sns_connect("").user_image("d:/upload/profile.png")
+            .user_delete_chk(false).user_create_date(LocalDateTime.now())
+            .build();
+
+            userRepository.save(user);
+
+        }
+    }
+    
+}
