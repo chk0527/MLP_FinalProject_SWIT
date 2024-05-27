3298c69835344db496d24423e71ca1448060f79c
SihoonKim <ch10152080@gmail.com>
2024-05-27 11:25:03 +0900

    스터디 데이터베이스 저장 테스트

--- back/swit/src/main/java/com/swit/swit/domain/Study.java
+++ back/swit/src/main/java/com/swit/swit/domain/Study.java
@@ -1,17 +1,26 @@
-package com.swit.dto;
+package com.swit.swit.domain;
 
 import java.time.LocalDate;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name = "study")
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class StudyDTO {
+public class Study {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer studyNo;
     private String studyTitle;
     private String studyContent;
@@ -20,7 +29,7 @@ public class StudyDTO {
     private LocalDate studyEndDate;
     private Integer studyHeadcount;
     private Boolean studyOnlineChk;
-    private String studySubjeck;
+    private String studySubject;
     private String studyComm;
     private String studyLink;
 }
---
+++ back/swit/src/main/java/com/swit/swit/repository/StudyRepository.java
@@ -0,0 +1,9 @@
+package com.swit.swit.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.swit.swit.domain.Study;
+
+public interface StudyRepository extends JpaRepository<Study, Integer> {
+
+}
