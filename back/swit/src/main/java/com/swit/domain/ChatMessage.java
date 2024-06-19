9c4c93b98f12ada50c34ad66b39e9789cd5d0788
HyeonKyuChoi <chlgusrb0213@naver.com>
2024-06-19 17:32:31 +0900

    FEAT: 그룹 가입 시 상태 확인

--- back/swit/src/main/java/com/swit/domain/ChatMessage.java
+++ back/swit/src/main/java/com/swit/domain/ChatMessage.java
@@ -1,10 +1,17 @@
 package com.swit.domain;
 
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -27,6 +34,14 @@ public class ChatMessage {
 
     @Column(nullable = false)
     private String message;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id",referencedColumnName="userId")
+    private User user;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdDate;
 
     public ChatMessage(Long studyNo, String name, String message) {
         this.studyNo = studyNo;
