50107bcff6fbe39b9eb844fd38da92690fce73e6
kim-youngwook <rfonline64@gmail.com>
2024-07-02 15:50:06 +0900

    비밀번호 찾기 수정

--- front/swit/src/components/login/SearchIdComponent.js
+++ front/swit/src/components/login/SearchIdComponent.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { searchId, searchId2, send_sms } from "../../api/LoginApi"
 
 const initState = { 
@@ -19,7 +19,8 @@ function SearcIdComponent() {
   const [emailDetail, setEmailDetail] = useState('');
   const [emailDomain, setEmailDomain] = useState('');
   const [userEmail, setUserEmail] = useState('');
-
+  const [isVerified, setIsVerified] = useState(false);
+  const [isNotVerified, setIsNotVerified] = useState(false);
 
   const [mobilePrefix, setMobilePrefix] = useState('010');
   const [mobile1, setMobile1] = useState('');
@@ -32,64 +33,109 @@ function SearcIdComponent() {
 
   const [isVerificationCodeSent, setIsVerificationCodeSent] = useState(false);
   const [timeRemaining, setTimeRemaining] = useState(300); // 5분 (300초)
-  const [verificationTimeout, setVerificationTimeout] = useState(null);
+  const [verificationTimeout, setVerificationTimeout] = useState('');
+
+  useEffect(() => {
+    if (emailDetail.trim()) {
+      setUserEmail(`${emailHead.trim()}@${emailDetail.trim()}`);
+    } else {
+      setUserEmail(`${emailHead.trim()}@${emailDomain.trim()}`);
+    }
+  }, [emailHead, emailDetail, emailDomain]);
 
   const handleCertifyTypeChange = (e) => {
     setCertifyType(e.target.value);
+    setIsVerified(false);
+    setIsNotVerified(false);
   };
 
   const handleNameChange = (e) => {
     setName(e.target.value);
+    setIsVerified(false);
+    setIsNotVerified(false);
   };
 
   const handleEmailHeadChange = (e) => {
     setEmailHead(e.target.value);
+    setIsVerified(false);
+    setIsNotVerified(false);
   };
 
   const handleEmailDetailChange = (e) => {
     setEmailDetail(e.target.value);
     setEmailDomain("");
+    setIsVerified(false);
+    setIsNotVerified(false);
   };
 
   const handleEmailDomainChange = (e) => {
     setEmailDomain(e.target.value);
     setEmailDetail("");
+    setIsVerified(false);
+    setIsNotVerified(false);
   };
 
   const handleMobilePrefixChange = (e) => {
     setMobilePrefix(e.target.value);
+    setIsVerified(false);
+    setIsNotVerified(false);
   };
 
   const handleMobile1Change = (e) => {
     setMobile1(e.target.value);
+    setIsVerified(false);
+    setIsNotVerified(false);
   };
 
   const handleMobile2Change = (e) => {
     setMobile2(e.target.value);
+    setIsVerified(false);
+    setIsNotVerified(false);
   };
 
   const handleConfirmNumChange = (e) => {
     setConfirmNum(e.target.value);
+    setIsVerified(false);
+    setIsNotVerified(false);
+
   };
 
   const handleSendVerificationCode = (e) => {
     // 실제 인증 코드 전송 로직 작성
-    console.log('인증 코드 전송 timeRemaining' + timeRemaining);
+    // console.log('인증 코드 전송 timeRemaining' + timeRemaining);
     // setTimeRemaining(300); // 남은 시간 5분으로 초기화
+    setIsVerified(false);
+    setIsNotVerified(false);
     
     // 이름 필수 입력 체크
     if (!name.trim()) {
-      alert('이름을 입력해 주세요.111');
+      alert('이름을 입력해 주세요.');
       return;
     }
-    // 휴대폰 번호 필수 입력 체크
-    if (!mobile1.trim() || !mobile2.trim()) {
-      alert('휴대폰 번호를 입력해 주세요.');
-      return;
+    if (certifyType === '1' ) {
+      
+      // if (!emailHead.trim() || (!emailDetail.trim() && !emailDomain.trim())) {
+      if (!emailHead.trim() || (!emailDetail.trim() && !emailDomain.trim())) {
+          alert('이메일은 필수 입력 항목입니다.');
+          return false;
+      }
+      if (!/\S+@\S+\.\S+/.test(userEmail)) {
+        alert('이메일 형식이 올바르지 않습니다.');
+        return false;
+      }
+
+    } else if (certifyType === '2' ) {
+      // 휴대폰 번호 필수 입력 체크
+      if (!mobile1.trim() || !mobile2.trim()) {
+        alert('휴대폰 번호를 입력해 주세요.');
+        return;
+      }
     }
 
-    // setUserEmail(`${emailHead}@${emailDomain}`);
-    searchId(certifyType, "", name, `${emailHead}@${emailDomain}`, `${mobilePrefix}${mobile1.trim()}${mobile2.trim()}` )
+    // 회원 여부 확인
+    // 이메일 검증인 경우 이메일 발송 포함
+
+    searchId(certifyType, "", name, userEmail, `${mobilePrefix}${mobile1.trim()}${mobile2.trim()}` )
     .then((response) => {
       
       console.log("sms 발송 nework에서 확인" + response.data);
@@ -101,28 +147,39 @@ function SearcIdComponent() {
       console.log(`setConfirm ${confirm.confirmPath}`);
       console.log(`setConfirm ${confirm.confirmNum}`);
       console.log(`setConfirm ${confirm.confirmLimitDate}`);
+      if (certifyType === '1' ) {
+        console.log("이메일 인증번호 발송이 성공하였습니다.");
+            
+        clearVerificationTimer();             // 초기화
+        setIsVerificationCodeSent(true);      // 활성화
+        startVerificationTimer();             // 타이머 카운트 시작
+      }
       // 고객정보 확인, 인증번호 얻기, sms 발송
-      send_sms(response.data)
-      .then((result) => {
-          console.log("인증번호 발송이 성공하였습니다.");
-          
-
-          clearVerificationTimer();             // 초기화
-          setIsVerificationCodeSent(true);      // 활성화
-          startVerificationTimer();             // 타이머 카운트 시작
-      })
-      .catch((error) => {
-          console.log("인증번호 발송이 실패하였습니다.");
-          return;
-      });
+      if (certifyType === '2' ) {
+        send_sms(response.data)
+        .then((result) => {
+            console.log("SMS 인증번호 발송이 성공하였습니다.");
+            
+            clearVerificationTimer();             // 초기화
+            setIsVerificationCodeSent(true);      // 활성화
+            startVerificationTimer();             // 타이머 카운트 시작
+        })
+        .catch((error) => {
+            console.log("인증번호 발송이 실패하였습니다.");
+            return;
+        });
+      }
 
     })
     .catch((error) => {
-      alert("회원명, 핸드폰번호를 확인해 주세요.");
-      return;
+      if (certifyType === '1' ) {
+        alert("회원명, 이메일을 확인해 주세요.");
+        return; 
+      } else {
+        alert("회원명, 핸드폰번호를 확인해 주세요.");
+        return;
+      }
     });
-    
-
   };
 
   const startVerificationTimer = () => {
@@ -144,6 +201,15 @@ function SearcIdComponent() {
     // 실제 인증 코드 확인 로직 작성
     console.log('인증 코드 확인');
 
+    if (timeRemaining <= 0) {
+      alert('5분 지났습니다. 인증번호를 다시 받으시기 바랍니다.');
+      return;
+    }
+
+    if (!confirmNum.trim()) {
+      alert('인증번호를 입력해 주세요.');
+      return;
+    }
     try {
       console.log(`try ${confirm.confirmNo}`);
       console.log(`try ${confirm.userId}`);
@@ -160,20 +226,23 @@ function SearcIdComponent() {
         confirmLimitDate: confirm.confirmLimitDate
       })
       .then((response) => {
-        console.log("인증번호 정상 입니다.");
+        console.log("인증번호가 일치입니다.");
         setConfirm(response.data);
         setIsVerificationCodeSent(false);
-        alert("인증번호 정상입니다.");
+        alert("인증번호가 일치입니다.");
         // 인증 성공 시
         // setTimeRemaining(300); // 남은 시간 5분으로 초기화
         // clearVerificationTimer();
+        setIsVerified(true);
       })
       .catch((error) => {
-        alert("인증번호 틀렸습니다. 다시 입력해 주세요");
+        alert("인증번호 불일치 합니다. 다시 입력해 주세요");
+        setIsNotVerified(true);
         return;
       });
     } catch (error) {
-      alert("인증번호 틀렸습니다. 다시 입력해 주세요");
+      alert("인증번호 불일치 합니다. 다시 입력해 주세요");
+      setIsNotVerified(true);
     }
     
     // 인증 실패 시
@@ -203,23 +272,15 @@ function SearcIdComponent() {
           alert('이름을 입력해 주세요.');
           return;
         }
-        
-        if (!emailHead.trim()) {
-          alert('이메일을 입력해 주세요1.');
-          return;
+        if (!emailHead.trim() || (!emailDetail.trim() && !emailDomain.trim())) {
+            alert('이메일은 필수 입력 항목입니다.');
+            return false;
+        }
+        if (!/\S+@\S+\.\S+/.test(userEmail)) {
+          alert('이메일 형식이 올바르지 않습니다.');
+          return false;
         }
 
-        if (!emailDomain.trim()) {
-          if(!emailDetail.trim()) {
-            alert('이메일을 입력해 주세요.');
-            return;
-          } else {
-            setUserEmail(emailHead + "@" + emailDetail);
-          }
-
-        } else {
-          setUserEmail(emailHead + "@" + emailDetail);
-        }
 
         searchId(certifyType, "", name, userEmail, `${mobilePrefix}${mobile1.trim()}${mobile2.trim()}` )
         .then((response) => {
@@ -233,25 +294,38 @@ function SearcIdComponent() {
           return;
         });
 
+        if (`${confirm.confirmPath}` == '1') {  
+          console.log("이메일 인증번호 검증")
+
+        } else {
+          console.log("아이디 찾기가 이메일인지 확인해 주세요.");
+          return;
+        }
+
     } else {
-      if (`${confirm.confirmPath}` == '2') {
-        if (certifyType == '2') {
-            console.log("핸드폰 인증번호 검증")
-            // searchId2(certifyType, "", name, `${emailHead}@${emailDomain}`, `${mobilePrefix}${mobile1.trim()}${mobile2.trim()}` )
-            // .then((response) => {
-            //   // 고객정보 확인, 이메일 발송
-            //   console.log("이메일 검증 nework에서 확인" + response.data);
-            //   send_email(response.data)
-            // })
-            // .catch((error) => {
-            //   console.log("회원명, 이메일을 확인해 주세요.");
-            //   return;
-            // });
+
+        if (!confirm.userId.trim()) {
+          alert('SMS 인증 하시기 바랍니다.');
+          return;
+        }
+        // 이름 필수 입력 체크
+        if (!name.trim()) {
+          alert('이름을 입력해 주세요.');
+          return;
+        }
+        // 휴대폰 번호 필수 입력 체크
+        if (!mobile1.trim() || !mobile2.trim()) {
+          alert('휴대폰 번호를 입력해 주세요.');
+          return;
+        }
+
+        if (`${confirm.confirmPath}` == '2') {
+              console.log("핸드폰 인증번호 검증")
+      
         } else {
           console.log("아이디 찾기가 핸드폰인지 확인해 주세요.");
           return;
         }
-      }
     }
     setResultType("2");
   };
@@ -427,11 +501,21 @@ function SearcIdComponent() {
                         확인
                       </button>
                       {isVerificationCodeSent && (
-                        <div className="mt-2 text-gray-500">
+                        <div className="mt-2 px-4 text-gray-500">
                           남은 시간: {Math.floor(timeRemaining / 60)}분 {timeRemaining % 60}초
                         </div>
                       )}
                 </div>
+                {/* {isVerified && (
+                  <div className="text-red-500 font-medium">
+                    인증번호가 일치합니다.
+                  </div>
+                )}
+                {isNotVerified && (
+                  <div className="text-red-500 font-medium">
+                    인증번호가 불일치합니다.
+                  </div>
+                )} */}
                 <p className="mbrBtnFunc">
                 <span className="mbrBtn mbrBtnSearch_4">
                    <button
