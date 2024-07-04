4d0f816c361096b8d1d7da5f7fea8d6b482c3ed6
suminni <ban04026@gmail.com>
2024-07-04 15:29:54 +0900

    채용스케쥴링

--- back/swit/src/main/java/com/swit/SwitApplication.java
+++ back/swit/src/main/java/com/swit/SwitApplication.java
@@ -2,12 +2,15 @@ package com.swit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.StringHttpMessageConverter;
 // import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import com.swit.domain.User;
 
 import lombok.extern.log4j.Log4j2;
+import java.nio.charset.StandardCharsets;
 
 @Log4j2
 @SpringBootApplication
@@ -21,5 +24,10 @@ public class SwitApplication {
     user.setUserId("domain_test_name2");
     log.info("sample domain test" + user.getUserId());
   }
+
+  @Bean
+    public StringHttpMessageConverter stringHttpMessageConverter() {
+        return new StringHttpMessageConverter(StandardCharsets.UTF_8);
+    }
 
 }
--- front/swit/src/components/main/PlaceRecommend.js
+++ front/swit/src/components/main/PlaceRecommend.js
@@ -163,3 +163,4 @@ const PlaceRecommend = () => {
 };
 
 export default PlaceRecommend;
+
